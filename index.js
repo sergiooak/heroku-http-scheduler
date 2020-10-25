0e198bf5f9c916c76fa929223f4939d34c49d56e
Sergio Carvalho <sergiopcn@gmail.com>
2020-10-24 22:16:52 -0300

    dont't schedule if get respond with success

--- index.js
+++ index.js
@@ -7,26 +7,35 @@ app.use(bodyParser.json());
 
 app.get("/", function (req, res) {
   console.log("get");
-  res.send("http-scheduler v1.0.0");
+  res.send("http-scheduler v1.0.1");
 });
 
 app.post("/*", function (req, res) {
   console.log(`Sending wake up request to ${req.body.host}${req.originalUrl}`);
-  axios.get(`${req.body.host}${req.originalUrl}`).catch(function (error) {
-    console.log('The waking up got an error (like always)');
-  });
+  axios
+    .get(`${req.body.host}${req.originalUrl}`)
+    .then(() => {
+      // If success, send the next request imediatly
+      console.log("Making the real request right now");
+      axios
+        .post(`${req.body.host}${req.originalUrl}`, req.body)
+        .catch(function (error) {
+          console.log(error);
+        });
+    })
+    .catch((error) => {
+      console.log("Scheduling the real request to 30 seconds from now");
+      setTimeout(() => {
+        console.log("Making the real request after the 30 seconds wait");
+        axios
+          .post(`${req.body.host}${req.originalUrl}`, req.body)
+          .catch(function (error) {
+            console.log(error);
+          });
+      }, 1000 * 30);
+    });
 
-  console.log("Scheduling the real request to one minute from now");
-  setTimeout(() => {
-    console.log("Making the real request right now");
-    axios
-      .post(`${req.body.host}${req.originalUrl}`, req.body)
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, 1000 * 60);
-
-  res.send("http-scheduler v1.0.0");
+  res.send("http-scheduler v1.0.1");
 });
 
 let port = process.env.PORT || 3000;
